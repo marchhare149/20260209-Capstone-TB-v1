9c9269ef8482ac7216bd44936104a9f6b49c05f2
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 14:25:15 +0100

    add security groups

---
+++ sg.tf
@@ -0,0 +1,91 @@
+# ALB SG: allow HTTP from Internet
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "ALB allows HTTP from internet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "alb-sg" }
+}
+
+# WordPress SG: allow HTTP from ALB SG, SSH from Bastion SG (later)
+resource "aws_security_group" "wp_sg" {
+  name        = "wordpress-sg"
+  description = "WordPress allows HTTP from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "wordpress-sg" }
+}
+
+# Bastion SG: allow SSH from your IP (replace later)
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Bastion allows SSH from admin IP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["88.150.80.232/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "bastion-sg" }
+}
+
+# RDS SG: allow MySQL from WordPress SG
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "RDS allows MySQL from WordPress"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wp_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "rds-sg" }
+}
