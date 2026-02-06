7e7f4a277ec8a8d64aba7f3fa40a10e23cf6556d
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 17:52:39 +0100

    add key_name variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   type    = string
   default = "us-west-2"
 }
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "vockey"
+}
