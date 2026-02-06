153684048dcbce88d44247d3b7df1453e7dc2d9e
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 17:51:04 +0100

    add wordpress launch template

---
+++ launch-template.tf
@@ -0,0 +1,20 @@
+resource "aws_launch_template" "wordpress" {
+  name_prefix   = "wp-lt-"
+  image_id      = "ami-0d593311db5abb72b"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  user_data = base64encode(file("userdata.sh"))
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.wp_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "wordpress-asg"
+    }
+  }
+}
